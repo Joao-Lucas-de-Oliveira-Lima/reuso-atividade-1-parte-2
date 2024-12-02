c8a69af47360d269f06437b5e4ff93bca542b76a
João Lucas <joaolucasdeoliveiralima2@gmail.com>
2024-12-02 18:52:01 -0300

    fix: removendo pastas duplicadas

--- activity/src/main.rs
+++
@@ -1,9 +0,0 @@
-use::restaurant::constants::PI;
-use:: restaurant::back_of_house::take_care_trash;
-use:: restaurant::front_of_house::hosting::add_to_wait_list;
-
-fn main() {
-    println!("Valor de PI: {}", PI);
-    take_care_trash();
-    add_to_wait_list();
-}
